e540898fd1e2efdd69466e79bf7fda54f4875a29
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-09-23 11:34:55 -0400

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,10 +1,11 @@
-#lang setup/infotab
+#lang info
 
 (define name "bit-struct")
+(define collection "bit-struct")
 (define scribblings '(("manual.scrbl" ())))
 
 (define blurb '("Binary structs with conversion to/from bytes."))
-(define primary-file "bit-struct/main.rkt")
+(define primary-file "main.rkt")
 (define homepage "https://github.com/jpverkamp/bit-struct/")
 
 (define version "1.0")
